254a88a1b0a23d8c2c43bb7aabbd51a15094e9ed
MuhammadDan <daniyalk154330@gmail.com>
2025-07-18 11:01:37 +0500

    Fourth question done

---
+++ SubArray/Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void Subarray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int start = i;
+            for (int j=i;j<arr.length;j++){
+                int end=j;
+                for(int k=start;k<=end;k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+	public static void main(String[] args) {
+		int arr[] = {2,4,6,8,10};
+		Subarray(arr);
+	}
+}
+// subarray means continious ho yani aik array ho 1,2,3,4,5 ager mai bolo 1,2,3 to yai subarray hoa likn 
+// ager mai bolo 1,2,4 to yai subarray nahi hoa kyu kai 4 lagatar nahi ata pehlai 3 ata hai phir 4
+// 2,3,4 -->subarray 3,4,5--->subarray 1,2,3--->subarray 
