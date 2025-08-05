33215dc6d429588fbd9181432957761a634615d7
MuhammadDan <daniyalk154330@gmail.com>
2025-08-06 03:48:55 +0500

    Twelth question done

---
+++ Threesum/Main.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main
+{
+    public static List<List<Integer>> threesum(int[] arr){
+        List<List<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(arr[i]+arr[j]+arr[k]==0){
+                        List<Integer> triplet = Arrays.asList(arr[i], arr[j], arr[k]);
+                        result.add(triplet);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+	public static void main(String[] args) {
+		int arr[] = {-1,0,1,2,-1,4};
+		System.out.println(threesum(arr));
+	}
+}
